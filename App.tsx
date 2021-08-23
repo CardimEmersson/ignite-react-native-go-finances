52393aca6089697f23de4ef75047d4016491a6ab
Emersson <emerssonmota123@gmail.com>
2021-08-23 17:57:40 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import AppLoading from "expo-app-loading";
+import { LogBox } from "react-native";
+import { Dashboard } from "./src/screens/Dashboard";
+import theme from "./src/global/styles/theme";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+export default function App() {
+  LogBox.ignoreLogs(["Remote debugger"]);
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "https://github.com/CardimEmersson.png" }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Emersson</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+          type="total"
+        />
+      </HighlightCards>
+    </Container>
+  );
+}
